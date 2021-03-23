dfffd94eb95ae05aa32859193a412cc415f63190
mirandamanuel <49087932+mirandamanuel@users.noreply.github.com>
2021-03-23 12:42:00 -0700

    Finished CH. 25

--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -1,7 +1,10 @@
 package com.bignerdranch.android.photogallery.api
 
+
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Url
 
 interface FlickrApi {
     @GET(
@@ -12,5 +15,8 @@ interface FlickrApi {
             "&extras=url_s"
     )
     fun fetchPhotos(): Call<FlickrResponse>
+
+    @GET
+    fun fetchUrlBytes(@Url url: String): Call<ResponseBody>
 }
 
